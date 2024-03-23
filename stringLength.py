5f042e77a36441c542e81366737fa2fd333e2767
shivamsuyal <shivamsuyal25@gmail.com>
2024-03-23 22:36:47 +0530

    init

---
+++ stringLength.py
@@ -0,0 +1,14 @@
+'''
+    This function converts the input into string and gives the lenght of that string.
+    Written by Prajwal Suyal
+    Sap id: 500094479
+    
+    '''
+    
+    
+    def getStringLenght(txt):
+        if !isinstance(txt,str):
+          raise TypeError("Input must be a string")
+        txt_len = len(txt) 
+        print(f"The string '{data}' is of {txt_len} characters")
+        return True
