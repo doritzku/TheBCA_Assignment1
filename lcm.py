5cffde27b24c1a1d5c9b8f4230d44aff8a667d3c
Namratyagi <tyaginamra@gmail.com>
2024-03-24 23:08:53 +0530

    Added python code to check LCM and GCD.

---
+++ lcm.py
@@ -0,0 +1,10 @@
+def gcd(a, b):
+    """Calculate the greatest common divisor (GCD) of two numbers."""
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    """Calculate the least common multiple (LCM) of two numbers."""
+    return (a * b) // gcd(a, b)
+
