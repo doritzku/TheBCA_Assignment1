140f02576de63fd7c333f71f34e91ba9d831da22
Hardik Gupta <hadik500089339@gmail.com>
2024-03-24 12:04:46 +0530

    Adding Factorial function by Hardik Gupta

---
+++ Test_factorial_hardik.py
@@ -0,0 +1,26 @@
+'''
+This is a test code for factorial function using the unit test module
+Written by Dhruv Aggarwal
+Sap id: 500094811
+'''
+
+import unittest
+from factorial import factorial
+
+
+class TestFactorial(unittest.TestCase):
+    def test_factorial_positive_numbers(self):
+        self.assertEqual(factorial(0), 1)  # Testing factorial of 0
+        self.assertEqual(factorial(1), 1)  # Testing factorial of 1
+        self.assertEqual(factorial(5), 120)  # Testing factorial of 5
+
+    def test_factorial_negative_numbers(self):
+        with self.assertRaises(ValueError):  # Testing factorial of negative number
+            factorial(-5)
+
+    def test_large_factorial(self):
+        self.assertEqual(factorial(10), 3628800)  # Testing factorial of a large number (10)
+
+
+if __name__ == '__main__':
+    unittest.main()
